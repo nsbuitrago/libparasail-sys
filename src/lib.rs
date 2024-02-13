35c4ec466997ac2e8c76192d1a2621356717283c
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-02-13 13:13:16 -0600

    docs: fix typos in readme and finish docs in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-/// Introduction
+/// # Introduction
 /// [libparasail-sys]() is a Rust crate that provides FFI bindings to [parasail](), a SIMD C
 /// library for pairwise sequence alignment. This sub-crate uses an autotools build for the C
-/// library, and the build process is managed by [bindgen](). For safe bindings, please see
-/// [parasail-rs]().
+/// library, and [bindgen]() to automatically generate bindings. Safe bindings are planned
+/// and will be provided as a seperate crate in the future.
+///
+/// For mor information on parasail, see the original C library [documentation](https://github.com/jeffdaily/parasail).
 
 mod parasail_bindings {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
