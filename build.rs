f43cc9dc8b73ac46025160bad4f197b0e3116182
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-02-15 17:37:26 -0600

    feat: try system parasail before building by default or set env var to force using system parasail (#1)

--- build.rs
+++ build.rs
@@ -22,22 +22,44 @@ fn try_system_parasail() -> Result<pkg_config::Library, pkg_config::Error> {
 }
 
 fn main() {
-    // Use `PARASAIL_NO_VENDOR` to try and force to use system parasail.
-    // println!("cargo:rerun-if-env-changed=PARASAIL_NO_VENDOR");
-    // let force_no_vendor = env::var("PARASAIL_NO_VENDOR").is_ok();
-    // if force_no_vendor {
-    //  if try_system_parasail().is_err() {
-    //    panic!("Could not find system parasail");
-    //  }
-    //  return;
-    // }
-    //
+    let vendored = env::var("CARGO_FEATURE_VENDORED").is_ok();
+    let zlib_ng_compat = env::var("CARGO_FEATURE_ZLIB_NG_COMPAT").is_ok();
 
+    // Use `PARASAIL_NO_VENDOR` to try and force to use system parasail.
+    println!("cargo:rerun-if-env-changed=PARASAIL_NO_VENDOR");
+    let force_no_vendor = env::var("PARASAIL_NO_VENDOR").map_or(false, |v| v != "0");
+
+    if force_no_vendor {
+        if try_system_parasail().is_err() {
+            panic!(
+                "The environment variable `PARASAIL_NO_VENDOR` is set, but no system parasail was found.
+                Please install parasail or unset `PARASAIL_NO_VENDOR` or use `PARASAIL_NO_VENDOR=0`."
+            );
+        }
+
+        return;
+    }
+
+    let use_system_parasail = !vendored && !zlib_ng_compat;
+    if use_system_parasail && try_system_parasail().is_ok() {
+        // no issues with system parasail
+        return;
+    }
+
+    println!("cargo:rustc-cfg=parasail_vendored");
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let out_dir = env::var("OUT_DIR").unwrap();
+
+    if !Path::new("parasail").exists() {
+        let _ = Command::new("git")
+            .args(&["submodule", "update", "--init", "libgit2"])
+            .status();
+    }
+
     let parasail_dir = PathBuf::from(&project_dir).join("parasail")
         .canonicalize()
         .expect("Failed to find parasail directory");
+
+    let out_dir = env::var("OUT_DIR").unwrap();
     let parasail_build = PathBuf::from(&out_dir).join("parasail_build");
     std::fs::create_dir_all(&parasail_build).unwrap();
 
@@ -59,9 +81,8 @@ fn main() {
         .success(), "Failed to make"
     );
 
-
-    // println!("cargo:rustc-link-search=native={}", parasail_build.display());
-    // println!("cargo:rustc-link-lib=static=parasail");
+    println!("cargo:rustc-link-search=native={}", parasail_build.display());
+    println!("cargo:rustc-link-lib=static=parasail");
 
     let bindings = bindgen::Builder::default()
         .header(headers_path_str)
