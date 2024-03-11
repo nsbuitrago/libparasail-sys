7c4b892fba468cc941d746a5796d28a591ac2c3d
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-03-11 17:47:52 -0500

    docs: add parasail-rs to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! # Introduction
-//! [libparasail-sys]() is a Rust crate that provides FFI bindings to [parasail](), a SIMD C
-//! library for pairwise sequence alignment. Safe Rust bindings are planned
-//! and will be provided as a seperate crate in the future.
+//! [libparasail-sys]() is a Rust crate that provides FFI bindings to
+//! [parasail](https://github.com/jeffdaily/parasail), a SIMD C
+//! library for pairwise sequence alignment. You may be interested in its parent crate, [parasail-rs](https://github.com/nsbuitrago/parasail-rs).
 //!
-//! For mor information on parasail, see the original C library [documentation](https://github.com/jeffdaily/parasail).
+//! For mor information on parasail, see the original C library [repository](https://github.com/jeffdaily/parasail).
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
